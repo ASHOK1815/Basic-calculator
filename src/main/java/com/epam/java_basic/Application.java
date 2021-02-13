6b3d26ad104516816255056a165d6f65676411b0
Ashok <rupender20@gmail.com>
2021-02-13 22:38:17 +0530

    adding_file

---
+++ src/main/java/com/epam/java_basic/Application.java
@@ -0,0 +1,14 @@
+package com.epam.java_basic;
+
+/**
+ * Application's entry point, use it to demonstrate your code execution
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        
+    	
+    	
+    }
+
+}
