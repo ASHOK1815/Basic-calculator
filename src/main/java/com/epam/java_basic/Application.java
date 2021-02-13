8bea08adf96153bd870dfea8af0509df0e8aa17b
Javedali0563 <javedali0563@gmail.com>
2021-02-13 23:07:38 +0530

    some msg

--- src/main/java/com/epam/java_basic/Application.java
+++ src/main/java/com/epam/java_basic/Application.java
@@ -7,8 +7,15 @@ public class Application {
 
     public static void main(String[] args) {
         
-    	
+    	  Result result = JUnitCore.runClasses(CalculatorTestCase.class);
+		
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+   }
     	
     }
 
-}
+
--- src/main/java/com/epam/java_basic/calculator/Calculator.java
+++ src/main/java/com/epam/java_basic/calculator/Calculator.java
@@ -3,23 +3,31 @@ package com.epam.java_basic.calculator;
 public class Calculator {
 
     public Calculator(int precision) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        // throw new UnsupportedOperationException("You need to implement this method");
     }
 
     public double add(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        int temp = a + b;
+        return (double) Math.round(temp * 100) / 100;
+        // throw new UnsupportedOperationException("You need to implement this method");
     }
 
     public double subtract(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        int temp = a - b;
+        return (double) Math.round(temp * 100) / 100;
+        // throw new UnsupportedOperationException("You need to implement this method");
     }
 
     public double multiply(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        int temp = a * b;
+        return (double) Math.round(temp * 100) / 100;
+        // throw new UnsupportedOperationException("You need to implement this method");
     }
 
     public double div(double a, double b) {
-        throw new UnsupportedOperationException("You need to implement this method");
+        double temp = a / b;
+        return (double) Math.round(temp * 100) / 100;
+        // throw new UnsupportedOperationException("You need to implement this method");
     }
 
 }
